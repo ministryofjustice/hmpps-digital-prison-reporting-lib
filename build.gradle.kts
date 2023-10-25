c9513f5a4630f172e5857152993b8beae5ea5ad5
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2023-10-25 11:06:16 +0100

    DPR2-177 update lib version to include jwt token fix in new version

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ publishing {
         group = "uk.gov.justice.service.hmpps"
         name.set(base.archivesName)
         artifactId = base.archivesName.get()
-        version = "1.0.0-rc.4"
+        version = "1.0.0-rc.5"
         description.set("A Spring Boot reporting library to be integrated into your project and allow you to produce reports.")
         url.set("https://github.com/ministryofjustice/hmpps-digital-prison-reporting-mi")
         licenses {
