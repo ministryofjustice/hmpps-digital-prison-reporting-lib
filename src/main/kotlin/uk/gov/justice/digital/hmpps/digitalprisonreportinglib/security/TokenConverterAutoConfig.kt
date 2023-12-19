e32293ca32f3e2b81953254f8d035c718f655325
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-12-19 07:58:46 +0000

    [DPR-000] More bean conflicts

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/TokenConverterAutoConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/TokenConverterAutoConfig.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration
 class TokenConverterAutoConfig {
 
   @Bean
+  @ConditionalOnBean(CaseloadProvider::class)
   @ConditionalOnMissingBean(DprAuthAwareTokenConverter::class)
   fun dprAuthAwareTokenConverter(
     caseloadProvider: CaseloadProvider,
