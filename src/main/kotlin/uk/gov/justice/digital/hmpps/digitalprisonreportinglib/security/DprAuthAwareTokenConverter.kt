4ce5416674914a76e5cd1ed34dae3217908f4627
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-12-20 16:06:20 +0000

    Revert "[DPR-000] Allow non-DPR auth tokens to be used"
    This reverts commit 8c8a5d02c8f61af1c5771fe88660bd22713a52d8.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprAuthAwareTokenConverter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprAuthAwareTokenConverter.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security
 
 import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.oauth2.jwt.Jwt
 
 interface DprAuthAwareTokenConverter : Converter<Jwt, DprAuthAwareAuthenticationToken>
