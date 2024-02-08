ea80619203171f3b3068e8c5c2d5b6695b06ca2c
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-02-08 09:53:44 +0000

    DPR2-341 linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/ConfiguredApiRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/ConfiguredApiRepository.kt
@@ -38,9 +38,9 @@ class ConfiguredApiRepository {
   ): List<Map<String, Any?>> {
     val stopwatch = StopWatch.createStarted()
     val jdbcTemplate = populateJdbcTemplate(dataSourceName)
-    //The result of the query can contain null values.
-    //This is coming from Java and if the returned type is not specified in Kotlin it will assume it is List<Map<String, Any>>
-    //while in reality it is List<Map<String, Any?>>.
+    // The result of the query can contain null values.
+    // This is coming from Java and if the returned type is not specified in Kotlin it will assume it is List<Map<String, Any>>
+    // while in reality it is List<Map<String, Any?>>.
     val result: List<Map<String, Any?>> = jdbcTemplate.queryForList(
       buildFinalQuery(
         buildReportQuery(query),
