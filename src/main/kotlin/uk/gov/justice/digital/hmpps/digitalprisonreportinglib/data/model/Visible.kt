5f4c5742852a306ebbe6d8c26cea4276634fbbe8
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-02-22 12:44:22 +0000

    DPR2-426 use annotations instead of custom serializer

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/Visible.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/Visible.kt
@@ -1,9 +1,26 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model
 
-import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.google.gson.annotations.SerializedName
 
-enum class Visible(@JsonValue val value: String) {
-  TRUE("true"),
-  FALSE("false"),
-  MANDATORY("mandatory"),
+enum class Visible {
+  @SerializedName(Companion.TRUE)
+  @JsonProperty(Companion.TRUE)
+  TRUE,
+
+  @SerializedName(Companion.FALSE)
+  @JsonProperty(Companion.FALSE)
+  FALSE,
+
+  @SerializedName(Companion.MANDATORY)
+  @JsonProperty(Companion.MANDATORY)
+  MANDATORY,
+
+  ;
+
+  companion object {
+    private const val TRUE: String = "true"
+    private const val FALSE: String = "false"
+    private const val MANDATORY: String = "mandatory"
+  }
 }
