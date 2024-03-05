a3e4dda810f86a858a5832be1908cdbf90d2efb4
stalwartsolutions <stalwartsolutions@users.noreply.github.com>
2024-03-05 11:59:29 +0000

    Linting fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/Count.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/Count.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.model
 
 import io.swagger.v3.oas.annotations.media.Schema
-import org.springframework.http.HttpStatus
 
 data class Count(
   @Schema(example = "501", description = "The total number of records")
