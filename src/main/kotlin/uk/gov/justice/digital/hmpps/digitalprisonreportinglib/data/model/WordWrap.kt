13f9a0563837c8adadec231fb2b87a64c5316120
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-03-21 13:52:14 +0000

    [DPR2-494] Wordwrap fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/WordWrap.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/WordWrap.kt
@@ -1,5 +1,14 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model
 
-enum class WordWrap {
+import com.google.gson.annotations.SerializedName
+
+enum class WordWrap() {
+  @SerializedName("none")
   None,
+
+  @SerializedName("normal")
+  Normal,
+
+  @SerializedName("break-words")
+  BreakWords
 }
