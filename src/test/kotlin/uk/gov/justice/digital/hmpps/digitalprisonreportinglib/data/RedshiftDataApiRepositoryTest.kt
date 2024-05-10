eeb277d4a34a512e26b2acf5e7a865992a25b2fa
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-05-10 16:12:31 +0100

    Dpr2 729 no filters validation error (#152)
    * DPR2-729 do not provide sql parameter if there are no filters
    
    * DPR2-729 changelog
    
    * Linting fixes
    
    ---------
    
    Co-authored-by: gavriil-g-moj <gavriil-g-moj@users.noreply.github.com>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/RedshiftDataApiRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/RedshiftDataApiRepositoryTest.kt
@@ -3,6 +3,9 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
+import org.mockito.kotlin.times
 import org.mockito.kotlin.whenever
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
 import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementRequest
@@ -10,6 +13,7 @@ import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementRespo
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementRequest
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementResponse
 import software.amazon.awssdk.services.redshiftdata.model.SqlParameter
+import software.amazon.awssdk.services.redshiftdata.model.ValidationException
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepositoryTest.Companion.REPOSITORY_TEST_DATASOURCE_NAME
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepositoryTest.Companion.REPOSITORY_TEST_POLICY_ENGINE_RESULT
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepositoryTest.Companion.REPOSITORY_TEST_QUERY
@@ -18,13 +22,7 @@ import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.Stateme
 
 class RedshiftDataApiRepositoryTest {
 
-  @Test
-  fun `executeQueryAsync should call the redshift data api with the correct query and return the execution id`() {
-    val redshiftDataClient = mock<RedshiftDataClient>()
-    val executeStatementRequestBuilder = mock<ExecuteStatementRequest.Builder>()
-    val executeStatementResponse = mock<ExecuteStatementResponse>()
-    val redshiftDataApiRepository = RedshiftDataApiRepository(redshiftDataClient, executeStatementRequestBuilder)
-
+  companion object {
     val sqlStatement = """WITH dataset_ AS (SELECT prisoners.number AS prisonNumber,CONCAT(CONCAT(prisoners.lastname, ', '), substring(prisoners.firstname, 1, 1)) AS name,movements.time AS date,movements.direction,movements.type,movements.origin,movements.origin_code,movements.destination,movements.destination_code,movements.reason
 FROM datamart.domain.movement_movement as movements
 JOIN datamart.domain.prisoner_prisoner as prisoners
@@ -32,6 +30,15 @@ ON movements.prisoner = prisoners.id),policy_ AS (SELECT * FROM dataset_ WHERE (
 SELECT *
           FROM filter_ ORDER BY date asc;
     """.trimMargin()
+  }
+
+  @Test
+  fun `executeQueryAsync should call the redshift data api with the correct query and return the execution id`() {
+    val redshiftDataClient = mock<RedshiftDataClient>()
+    val executeStatementRequestBuilder = mock<ExecuteStatementRequest.Builder>()
+    val executeStatementResponse = mock<ExecuteStatementResponse>()
+    val redshiftDataApiRepository = RedshiftDataApiRepository(redshiftDataClient, executeStatementRequestBuilder)
+
     val mockedBuilderWithSql = ExecuteStatementRequest.builder()
       .clusterIdentifier("ab")
       .database("cd")
@@ -109,5 +116,64 @@ SELECT *
     val actual = redshiftDataApiRepository.getStatementStatus(statementId)
 
     assertEquals(expected, actual)
+  }
+
+  @Test
+  fun `executeQueryAsync should call the redshift data api and not error when no filters are provided`() {
+    val redshiftDataClient = mock<RedshiftDataClient>()
+    val executeStatementRequestBuilder = mock<ExecuteStatementRequest.Builder>()
+    val executeStatementResponse = mock<ExecuteStatementResponse>()
+    val redshiftDataApiRepository = RedshiftDataApiRepository(redshiftDataClient, executeStatementRequestBuilder)
+    val finalQuery =
+      """WITH dataset_ AS (SELECT prisoners.number AS prisonNumber,CONCAT(CONCAT(prisoners.lastname, ', '), substring(prisoners.firstname, 1, 1)) AS name,movements.time AS date,movements.direction,movements.type,movements.origin,movements.origin_code,movements.destination,movements.destination_code,movements.reason
+FROM datamart.domain.movement_movement as movements
+JOIN datamart.domain.prisoner_prisoner as prisoners
+ON movements.prisoner = prisoners.id),policy_ AS (SELECT * FROM dataset_ WHERE (origin_code IN ('HEI','LWSTMC','NSI','LCI','TCI') AND lower(direction)='out') OR (destination_code IN ('HEI','LWSTMC','NSI','LCI','TCI') AND lower(direction)='in')),filter_ AS (SELECT * FROM policy_ WHERE TRUE)
+SELECT *
+          FROM filter_ ORDER BY date asc;
+      """.trimIndent()
+
+    val mockedBuilderWithSql = ExecuteStatementRequest.builder()
+      .clusterIdentifier("ab")
+      .database("cd")
+      .secretArn("ef")
+      .sql(finalQuery)
+
+    whenever(
+      executeStatementRequestBuilder.sql(
+        finalQuery,
+      ),
+    ).thenReturn(
+      mockedBuilderWithSql,
+    )
+
+    val queryParams = emptyList<SqlParameter>()
+    whenever(
+      executeStatementRequestBuilder.parameters(queryParams),
+    ).thenThrow(ValidationException::class.java)
+
+    whenever(
+      redshiftDataClient.executeStatement(
+        mockedBuilderWithSql.build(),
+      ),
+    ).thenReturn(executeStatementResponse)
+
+    whenever(
+      executeStatementResponse.id(),
+    ).thenReturn("someId")
+
+    val actual = redshiftDataApiRepository.executeQueryAsync(
+      query = REPOSITORY_TEST_QUERY,
+      filters = emptyList(),
+      sortColumn = "date",
+      sortedAsc = true,
+      reportId = EXTERNAL_MOVEMENTS_PRODUCT_ID,
+      policyEngineResult = REPOSITORY_TEST_POLICY_ENGINE_RESULT,
+      dataSourceName = REPOSITORY_TEST_DATASOURCE_NAME,
+    )
+
+    verify(executeStatementRequestBuilder, times(0)).parameters(any<List<SqlParameter>>())
+
+    assertEquals("someId", actual)
   }
 }
