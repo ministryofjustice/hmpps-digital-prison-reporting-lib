dc982183150d9b8ede4d8ca019cfd8f2a26f68cc
gavriil-g-moj <gavriil-g-moj@users.noreply.github.com>
2024-07-05 08:31:58 +0000

    Linting fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/service/ReportDefinitionMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/service/ReportDefinitionMapper.kt
@@ -102,7 +102,7 @@ class ReportDefinitionMapper(val configuredApiService: ConfiguredApiService) {
         reportVariantId,
         userToken,
         dataProductDefinitionsPath,
-        filterDatasets
+        filterDatasets,
       ) + maybeConvertToReportFieldsFromParameters(parameters),
     )
   }
