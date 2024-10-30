920b35d4586bff5bc4fe132f77b5797cd9384007
Stu Pegg <110236995+stalwartstu@users.noreply.github.com>
2024-10-30 16:06:20 +0000

    [DPR2-1355] Definitions API Auth (#413)
    * [DPR2-1355] Definitions API Auth
    
    * Linting fixes
    
    * Empty Commit
    
    ---------
    
    Co-authored-by: stalwartstu <stalwartstu@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/ClientDataProductDefinitionsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/ClientDataProductDefinitionsRepository.kt
@@ -2,10 +2,16 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data
 
 import com.google.common.cache.Cache
 import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ProductDefinition
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
+import java.net.URI
 
 class ClientDataProductDefinitionsRepository(
   private val dataProductDefinitionsClient: RestTemplate,
@@ -21,9 +27,14 @@ class ClientDataProductDefinitionsRepository(
       path?.let { path -> cache.getIfPresent(path) }
     }
     cachedDefinitions?.let { return it }
+
+    val authentication = SecurityContextHolder.getContext().authentication as DprAuthAwareAuthenticationToken
     val respEntity: ResponseEntity<List<ProductDefinition>>? = path?.let {
+      val headers: MultiValueMap<String, String> = HttpHeaders()
+      headers.add("Authorization", "Bearer ${authentication.jwt.tokenValue}")
+      val requestEntity = RequestEntity<List<ProductDefinition>>(headers, HttpMethod.GET, URI("$definitionsHost/$path"))
       dataProductDefinitionsClient
-        .exchange("$definitionsHost/$path", HttpMethod.GET, null, object : ParameterizedTypeReference<List<ProductDefinition>>() {})
+        .exchange(requestEntity, object : ParameterizedTypeReference<List<ProductDefinition>>() {})
     }
     return respEntity?.body?.let { responseBody ->
       definitionsCache?.put(path, responseBody)
