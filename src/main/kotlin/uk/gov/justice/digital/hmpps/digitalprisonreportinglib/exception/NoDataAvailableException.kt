aea0823dd43be864a99d9569e656029c47af48d4
djcostin <daniel.costin@gmail.com>
2024-12-04 10:49:46 +0000

    DPR2-1521 refactor exception

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/exception/NoDataAvailableException.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/exception/NoDataAvailableException.kt
@@ -2,4 +2,3 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.exception
 
 class NoDataAvailableException(val reason: String) : RuntimeException(reason)
 
-class UserAuthorisationException(val reason: String) : RuntimeException(reason)
