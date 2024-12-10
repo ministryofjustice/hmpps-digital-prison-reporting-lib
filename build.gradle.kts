b1a2a70c26586afd307198226ecc1b1711ed470a
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-12-10 08:58:27 +0000

    [DPR2-1514] Fix dependency issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
