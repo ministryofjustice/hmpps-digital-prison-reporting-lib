527bdd1575dae509196a18755fa3fe23bdc469a1
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-12-10 07:54:09 +0000

    [DPR2-1514] Small config fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/DynamoDbConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/DynamoDbConfig.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.config
 
 import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
@@ -13,10 +12,7 @@ class DynamoDbConfig {
   @Bean
   @ConditionalOnProperty("dpr.lib.dataproductdefinitions.dynamodb.enabled", havingValue = "true")
   @ConditionalOnMissingBean(DynamoDbClient::class)
-  fun dynamoDbClient(
-    @Value("\${dpr.lib.dataProductDefinitions.dynamoDbTable}") dynamoDbTable: String,
-  ): DynamoDbClient {
-    return DynamoDbClient.builder()
-      .build()
+  fun dynamoDbClient(): DynamoDbClient {
+    return DynamoDbClient.builder().build()
   }
 }
