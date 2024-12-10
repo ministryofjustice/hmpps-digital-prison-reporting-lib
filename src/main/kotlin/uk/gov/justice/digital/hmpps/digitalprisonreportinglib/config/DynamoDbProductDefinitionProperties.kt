9c661364b4e7e193a465b1358d26ed8a8e7e7bf4
Stu Pegg <110236995+stalwartstu@users.noreply.github.com>
2024-12-10 13:41:11 +0000

    [DPR2-1514] Fix DDB cross-account issue (#496)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/DynamoDbProductDefinitionProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/DynamoDbProductDefinitionProperties.kt
@@ -5,8 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties("dpr.lib.dataproductdefinitions.dynamodb")
 class DynamoDbProductDefinitionProperties(
   val region: String = "eu-west-2",
+  val accountId: String = "771283872747",
   val tableName: String = "dpr-data-product-definition",
   val categoryFieldName: String = "category",
   val definitionFieldName: String = "definition",
   val categoryIndexName: String = "category-index",
-)
+) {
+  val tableArn: String = "arn:aws:dynamodb:${this.region}:${this.accountId}:table/${this.tableName}"
+}
