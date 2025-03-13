d0491851b4b229e1802532cb2a5afb20cabd84d9
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-03-13 18:14:20 +0000

    Refactor resource server configuration to use ConditionalOnExpression for improved condition handling

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthResourceConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthResourceConfiguration.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,7 +13,11 @@ import uk.gov.justice.hmpps.kotlin.auth.HmppsResourceServerConfiguration
 import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
 
 @Configuration("dprResourceServerConfiguration")
-@ConditionalOnProperty(name = ["dpr.lib.user.role", "spring.security.oauth2.resourceserver.jwt.jwk-set-uri", "dpr.lib.system.authentication.enabled"])
+@ConditionalOnExpression(
+  "!T(org.springframework.util.StringUtils).isEmpty('\$dpr.lib.user.role:}') " +
+    "&& !T(org.springframework.util.StringUtils).isEmpty('\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri:}') " +
+    "&& ('\${dpr.lib.system.authentication.enabled:false}' == 'true')",
+)
 @AutoConfigureBefore(WebMvcAutoConfiguration::class)
 class DprSystemAuthResourceConfiguration(
   private val userPermissionProvider: UserPermissionProvider,
