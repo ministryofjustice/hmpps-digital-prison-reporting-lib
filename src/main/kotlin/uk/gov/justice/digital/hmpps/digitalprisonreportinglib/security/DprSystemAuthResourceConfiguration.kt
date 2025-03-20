ff88a18401238410037668274e0ae804c961c120
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-03-20 16:35:04 +0000

    IR-938 Remove unnecessary AutoConfigureBefore annotation in DprSystemAuthResourceConfiguration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthResourceConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthResourceConfiguration.kt
@@ -1,9 +1,7 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -14,7 +12,6 @@ import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomize
 
 @Configuration("dprResourceServerConfiguration")
 @ConditionalOnProperty(name = ["dpr.lib.system.role", "spring.security.oauth2.resourceserver.jwt.jwk-set-uri"])
-@AutoConfigureBefore(WebMvcAutoConfiguration::class)
 class DprSystemAuthResourceConfiguration(
   private val userPermissionProvider: UserPermissionProvider,
   @Value("\${dpr.lib.system.role}") private val systemRole: String,
