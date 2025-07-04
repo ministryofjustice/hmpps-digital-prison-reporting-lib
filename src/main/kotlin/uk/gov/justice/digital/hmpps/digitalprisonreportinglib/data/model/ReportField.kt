9ff19844b23b55046ebe508fabf527766f3690a6
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-07-04 11:35:42 +0100

    DPR2-1860 changed sortdirection field name in DPD to be all lowercase - removed invalid comment

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/ReportField.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/ReportField.kt
@@ -13,7 +13,6 @@ data class ReportField(
   val defaultSort: Boolean = false,
   @SerializedName("sortdirection")
   val sortDirection: SortDirection? = null,
-  // Formula and visible are not used yet. This is pending ticket https://dsdmoj.atlassian.net/browse/DPR2-241
   val formula: String? = null,
   val visible: Visible? = null,
 ) : Identified() {
