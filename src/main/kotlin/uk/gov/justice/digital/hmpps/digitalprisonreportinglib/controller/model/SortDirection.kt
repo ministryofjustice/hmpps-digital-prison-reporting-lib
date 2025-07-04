db7426070dced4be464e34f920bce61a17fb45f8
James Reed <james.reed2@justice.gov.uk>
2025-07-04 08:59:50 +0100

    remove unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/SortDirection.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/SortDirection.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.model
 
-import com.fasterxml.jackson.annotation.JsonValue
 import com.google.gson.annotations.SerializedName
 
 enum class SortDirection {
