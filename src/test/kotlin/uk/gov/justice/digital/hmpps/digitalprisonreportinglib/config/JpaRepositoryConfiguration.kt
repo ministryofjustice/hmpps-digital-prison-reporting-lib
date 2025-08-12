b13bf9413b577fd8b72a8bbc1ad98f76a1404444
ReedSoftware <109068236+ReedSoftware@users.noreply.github.com>
2025-08-12 10:29:46 +0000

    Linting fixes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/JpaRepositoryConfiguration.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/config/JpaRepositoryConfiguration.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-
 @EnableJpaRepositories(
   basePackages = ["uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data"],
   entityManagerFactoryRef = "mainEntityManagerFactory",
