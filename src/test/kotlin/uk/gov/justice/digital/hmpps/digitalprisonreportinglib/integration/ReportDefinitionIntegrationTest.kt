b31ab6bf49dfd4ebd96bf297309364c071db7fdb
djcostin <daniel.costin@gmail.com>
2025-11-17 09:50:00 +0000

    DPR2-2335 fix tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/integration/ReportDefinitionIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/integration/ReportDefinitionIntegrationTest.kt
@@ -138,57 +138,6 @@ class ReportDefinitionIntegrationTest : IntegrationTestBase() {
     }
   }
 
-  class ClientReportDefinitionListTest : IntegrationTestBase() {
-
-    companion object {
-      @JvmStatic
-      @DynamicPropertySource
-      fun registerProperties(registry: DynamicPropertyRegistry) {
-        registry.add("dpr.lib.dataProductDefinitions.host") { "http://localhost:9999" }
-      }
-    }
-
-    @Test
-    fun `Definition list is returned as expected when the definitions are retrieved from a service endpoint call`() {
-      val result = webTestClient.get()
-        .uri { uriBuilder: UriBuilder ->
-          uriBuilder
-            .path("/definitions")
-            .queryParam("dataProductDefinitionsPath", "definitions/prisons/orphanage")
-            .build()
-        }
-        .headers(setAuthorisation(roles = listOf(authorisedRole)))
-        .exchange()
-        .expectStatus()
-        .isOk
-        .expectBodyList<ReportDefinitionSummary>()
-        .returnResult()
-
-      assertThat(result.responseBody).isNotNull
-      assertThat(result.responseBody).hasSize(1)
-      assertThat(result.responseBody).first().isNotNull
-
-      val definition = result.responseBody!!.first()
-
-      assertThat(definition.name).isEqualTo("External Movements")
-      assertThat(definition.description).isEqualTo("Reports about prisoner external movements")
-      assertThat(definition.variants).hasSize(3)
-      assertThat(definition.variants[0]).isNotNull
-      assertThat(definition.variants[1]).isNotNull
-
-      val lastMonthVariant = definition.variants[0]
-
-      assertThat(lastMonthVariant.id).isEqualTo("last-month")
-      assertThat(lastMonthVariant.name).isEqualTo("Last month")
-      assertThat(lastMonthVariant.description).isEqualTo("All movements in the past month")
-
-      val lastWeekVariant = definition.variants[1]
-      assertThat(lastWeekVariant.id).isEqualTo("last-week")
-      assertThat(lastWeekVariant.description).isEqualTo("All movements in the past week")
-      assertThat(lastWeekVariant.name).isEqualTo("Last week")
-    }
-  }
-
   class DynamoDbReportDefinitionListTest : IntegrationTestBase() {
 
     companion object {
