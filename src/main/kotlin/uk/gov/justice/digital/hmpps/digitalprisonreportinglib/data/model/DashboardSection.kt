3b24da66e15c34543c332640a423b305e31391d9
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2026-02-17 14:22:59 +0000

    DPR2-2631 make DashboardSection display field mandatory

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/DashboardSection.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/model/DashboardSection.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model
 
 data class DashboardSection(
   val id: String,
-  val display: String? = null,
+  val display: String,
   val description: String? = null,
   val visualisation: List<DashboardVisualisation>,
 )
