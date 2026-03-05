3b1c7f4230b948980ad054c1058919d731de9546
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2026-03-05 14:04:35 +0000

    updated changelog (#830)
    Co-authored-by: James Reed <109068236+ReedSoftware@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/DynamoDbProductDefinitionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/DynamoDbProductDefinitionRepository.kt
@@ -74,7 +74,6 @@ class DynamoDbProductDefinitionRepository(
     }
     // Make sure every path has results
     if (cachedDefinitions.all { it.isNotEmpty() }) {
-      log.debug("Getting product definitions from the cache.")
       return cachedDefinitions.flatten()
     }
 
@@ -90,9 +89,7 @@ class DynamoDbProductDefinitionRepository(
     var response = dynamoDbClient.scan(getScanRequest(properties, usePaths))
 
     while (response.hasLastEvaluatedKey()) {
-      scanStopwatch.suspend()
       addToDefinitionsMap(response, definitionMap)
-      scanStopwatch.resume()
       response = dynamoDbClient.scan(getScanRequest(properties, usePaths, response.lastEvaluatedKey()))
     }
     scanStopwatch.stop()
@@ -105,8 +102,11 @@ class DynamoDbProductDefinitionRepository(
     response.items()
       .filter { it[properties.dynamoDb.definitionFieldName] != null }
       .forEach {
+        val deserialisationStopwatch = StopWatch.createStarted()
         val definition =
           gson.fromJson(it[properties.dynamoDb.definitionFieldName]!!.s(), ProductDefinitionSummary::class.java)
+        deserialisationStopwatch.stop()
+        log.debug("Deserialisation of product definition {} took: {}", definition.id, deserialisationStopwatch.time)
         val definitionPath = it[properties.dynamoDb.categoryFieldName]!!.s()
         definition.path =
           DataDefinitionPath.entries.firstOrNull { path -> path.value == definitionPath } ?: DataDefinitionPath.OTHER
