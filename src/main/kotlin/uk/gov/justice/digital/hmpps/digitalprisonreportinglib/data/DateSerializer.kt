6df2fa7226283ac00a75196aaa4e409081cada92
madhuri.mahesh <madhuri.mahesh@justice.gov.uk>
2026-03-24 15:44:58 +0000

    DPR2-2520 - clean up

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/DateSerializer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/data/DateSerializer.kt
@@ -3,10 +3,15 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data
 import tools.jackson.core.JsonGenerator
 import tools.jackson.databind.SerializationContext
 import tools.jackson.databind.ser.std.StdSerializer
+import java.text.SimpleDateFormat
 import java.util.Date
 
 class DateSerializer : StdSerializer<Date>(Date::class.java) {
+
+  private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
   override fun serialize(value: Date, gen: JsonGenerator, ctxt: SerializationContext) {
-    gen.writeNumber(value.time) // milliseconds since epoch
+    val formattedDate = formatter.format(value)
+    gen.writeNumber(formattedDate)
   }
 }
