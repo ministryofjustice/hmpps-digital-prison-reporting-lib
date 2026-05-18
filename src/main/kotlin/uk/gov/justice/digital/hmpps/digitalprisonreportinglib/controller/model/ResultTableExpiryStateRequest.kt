4bc8746d4ce8195e81ca2044480734e16eb537c6
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2026-05-18 10:47:45 +0100

    DPR2-2799 Increased limit of the POST tableExpiryState endpoint to accept up to 200 tableIds from 50. (#941)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/ResultTableExpiryStateRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/controller/model/ResultTableExpiryStateRequest.kt
@@ -3,6 +3,6 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.model
 import jakarta.validation.constraints.Size
 
 data class ResultTableExpiryStateRequest(
-  @field:Size(max = 50)
+  @field:Size(max = 200)
   val tableIds: List<String>,
 )
