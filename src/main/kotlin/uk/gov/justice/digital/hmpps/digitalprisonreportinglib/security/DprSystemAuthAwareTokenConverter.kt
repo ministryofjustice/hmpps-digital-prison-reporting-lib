7df083a02575f951d704773e79eb7879b6f4fac6
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-05-27 15:34:55 +0100

    do authsource check when getting user info instead (#959)
    * do authsource check when getting user info instead
    
    * linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthAwareTokenConverter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportinglib/security/DprSystemAuthAwareTokenConverter.kt
@@ -6,33 +6,16 @@ import uk.gov.justice.hmpps.kotlin.auth.AuthAwareAuthenticationToken
 import uk.gov.justice.hmpps.kotlin.auth.AuthAwareTokenConverter
 import uk.gov.justice.hmpps.kotlin.auth.extractAuthorities
 
-class DprSystemAuthAwareTokenConverter(
-  private val requiredAuthSources: List<String>,
-) : AuthAwareTokenConverter() {
+class DprSystemAuthAwareTokenConverter : AuthAwareTokenConverter() {
 
-  override fun convert(jwt: Jwt): AuthAwareAuthenticationToken {
-    val token = super.convert(jwt)
-      .let { authAwareAuthenticationToken ->
-        DprSystemAuthAwareAuthenticationToken(
-          jwt,
-          authAwareAuthenticationToken.clientId,
-          authAwareAuthenticationToken.userName,
-          authAwareAuthenticationToken.authSource,
-          extractAuthorities(jwt, JwtGrantedAuthoritiesConverter()),
-        )
-      }
-
-    if (!requiredAuthSources.map { it.lowercase() }.contains(token.authSource.name.lowercase())) {
-      // Return a token with no authorities which will then be rejected
-      return DprSystemAuthAwareAuthenticationToken(
+  override fun convert(jwt: Jwt): AuthAwareAuthenticationToken = super.convert(jwt)
+    .let { authAwareAuthenticationToken ->
+      DprSystemAuthAwareAuthenticationToken(
         jwt,
-        token.clientId,
-        token.userName,
-        token.authSource,
-        emptyList(),
+        authAwareAuthenticationToken.clientId,
+        authAwareAuthenticationToken.userName,
+        authAwareAuthenticationToken.authSource,
+        extractAuthorities(jwt, JwtGrantedAuthoritiesConverter()),
       )
     }
-
-    return token
-  }
 }
